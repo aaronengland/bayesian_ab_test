e3495a0b663e47e2714a7d031ee4977306bb6417
Aaron England <aaron.england24@gmail.com>
2019-07-11 09:31:29 -0700

    added new count ab test function

--- bayesian_ab_test/__init__.py
+++ bayesian_ab_test/__init__.py
@@ -9,8 +9,206 @@ import pandas as pd
 from scipy.stats import ttest_ind
 import numpy as np
 
-# define function
-def bayesian_ab_test_prob(sample_a_total, sample_a_responses, sample_b_total, sample_b_responses, N_simulations=1000, pct_tune=50, gr_threshold=1, N_additional_draws=1000):
+# define function for testing counts
+def bayesian_ab_test_count(sample_A_n, sample_A_count, sample_B_n, sample_B_count, N_simulations=1000, pct_tune=50, gr_threshold=1.01, N_additional_draws=1000, lpv_height=15, n_x_observed=2):
+    # get parameters for model
+    # make pct_tune into a proportion
+    prop_tune = pct_tune/100
+    # calculate number to tune
+    N_tune = round(N_simulations*prop_tune)
+    # calculate additional tuning steps
+    N_additional_tune = round(N_additional_draws*prop_tune)
+    
+    ###########################################################################
+    # get data for lower and upper plausible values
+    # define helper function for plausible values
+    def plausible_values(total, upvotes):
+        # get downvotes
+        d = total - upvotes
+        # 1 + upvotes
+        a = 1 + upvotes
+        # 1 + downvotes
+        b = 1+ d
+        # calculate lower plausible value
+        lpv = (a/(a+b)) - (1.65 * np.sqrt((a*b)/(((a+b)**2)*(a+b+1))))
+        # calculate upper plausible value
+        upv = (a/(a+b)) + (1.65 * np.sqrt((a*b)/(((a+b)**2)*(a+b+1))))
+        # return lpv and upv
+        return lpv, upv
+    # sample A
+    # number of non-clicks
+    N_nonclicks_A = sample_A_n - sample_A_count
+    # calculate CTR
+    observed_p_A = sample_A_count/sample_A_n
+    # calculate lower plausible value in sample A
+    sample_A_lpv = plausible_values(total=sample_A_n, upvotes=sample_A_count)[0]
+    # calculate upper plausible value in sample A
+    sample_A_upv = plausible_values(total=sample_A_n, upvotes=sample_A_count)[1]
+    # sample B
+    # calculate non-clicks
+    N_nonclicks_B = sample_B_n - sample_B_count
+    # calculate CTR
+    observed_p_B = sample_B_count/sample_B_n
+    # calculate lower plausible value in sample B
+    sample_B_lpv = plausible_values(total=sample_B_n, upvotes=sample_B_count)[0]
+    # calculate upper plausible value in sample B
+    sample_B_upv = plausible_values(total=sample_B_n, upvotes=sample_B_count)[1]
+    
+    # put into df
+    df = pd.DataFrame({'Variable': ['Total','Yes','No','Rate','LPV','UPV'],
+                       'Sample A': [sample_A_n, sample_A_count, N_nonclicks_A, observed_p_A, sample_A_lpv, sample_A_upv],
+                       'Sample B': [sample_B_n, sample_B_count, N_nonclicks_B, observed_p_B, sample_B_lpv, sample_B_upv]})
+    # create a col that is sample A minus sample B
+    df['A - B'] = df['Sample A'] - df['Sample B']
+    
+    ###########################################################################
+    # plot it
+    x = ('Sample A', 'Sample B')
+    y = (observed_p_A, observed_p_B)
+    # get error values
+    # lower
+    # sample A
+    A_low_err = observed_p_A - sample_A_lpv
+    # sample B
+    B_low_err = observed_p_B - sample_B_lpv
+    # upper
+    # sample A
+    A_upp_err = sample_A_upv - observed_p_A
+    # sample B
+    B_upp_err = sample_B_upv - observed_p_B
+    # create a programmatic title
+    # print message
+    if sample_A_lpv > sample_B_lpv:
+        title = 'Sample A has a greater LPV'
+    else:
+        title = 'Sample B has a greater LPV'
+    yerr = np.array([(A_low_err, B_low_err), (A_upp_err, B_upp_err)])
+    lpv_plot, axes = plt.subplots()
+    axes.errorbar(x, y, yerr, fmt='o')
+    axes.set_title(title)
+    
+    ###########################################################################
+    # set up pymc3 model assuming uniform prior and Bernoulli posterior
+    # print a message
+    print('\n')
+    print('Model being built using {} initial, tuned draws...'.format(N_simulations))
+    print('\n')
+    
+    # instantiate model
+    with pm.Model() as model:
+        # get prior probabilities from Uniform distribution because we don't know what they are (objective prior)
+        prior_A_count = pm.Uniform('prior_A_count', 0, n_x_observed*sample_A_count)
+        prior_B_count = pm.Uniform('prior_B_count', 0, n_x_observed*sample_B_count)
+        prior_A_n = pm.Uniform('prior_A_n', 0, n_x_observed*sample_A_n)
+        prior_B_n = pm.Uniform('prior_B_n', 0, n_x_observed*sample_B_n)
+        # get posterior probabilities
+        post_A_count = pm.Poisson('post_A_count', prior_A_count, observed=sample_A_count)
+        post_B_count = pm.Poisson('post_B_count', prior_B_count, observed=sample_B_count)
+        post_A_n = pm.Poisson('post_A_n', prior_A_n, observed=sample_A_n)
+        post_B_n = pm.Poisson('post_B_n', prior_B_n, observed=sample_B_n)
+        # get samples from the posterior distribution
+        trace = pm.sample(draws=N_simulations+N_tune, tune=N_tune)
+        
+    # get maximum value of Gelman-Rubin test
+    max_gr = max(np.max(gr_stats) for gr_stats in pm.gelman_rubin(trace).values()) 
+    # if model has not converged, continue to...
+    while max_gr > gr_threshold:
+        # print message
+        print('\n')
+        print('Gelman-Rubin statistic: {}'.format(max_gr))
+        print('Gelman-Rubin statistic is too large, {} additional draws will be taken.'.format(N_additional_draws))
+        print('\n')
+        with model:
+            trace = pm.sample(draws=N_additional_draws+N_additional_tune, tune=N_additional_tune)
+        # get maximum value of Gelman-Rubin test
+        max_gr = max(np.max(gr_stats) for gr_stats in pm.gelman_rubin(trace).values())
+        # add N_additional_draws to N_simulations
+        N_simulations += N_additional_draws
+    # print message
+    print('\n')
+    print('Success! Model has converged after {} draws. Final Gelman-Rubin: {}'.format(N_simulations, max_gr))
+        
+    ###########################################################################
+    # display conversion stats
+    # calculate the bayesian fraction of missing information
+    bfmi = pm.bfmi(trace)
+    # get maximum value of Gelman-Rubin test
+    max_gr = max(np.max(gr_stats) for gr_stats in pm.gelman_rubin(trace).values())
+    # print message
+    print('Bayesian fraction of missing information: {}'.format(bfmi))
+    print('\n')
+    
+    ###########################################################################
+    # get distributions
+    # calculate count/n
+    prior_A_count = trace['prior_A_count']
+    prior_B_count = trace['prior_B_count']
+    prior_A_n = trace['prior_A_n']
+    prior_B_n = trace['prior_B_n']
+    
+    # instantiate empty lists
+    sample_A_count_div_n_list = []
+    sample_B_count_div_n_list = []
+    for i in range(0, len(prior_A_count)):
+        # calculate count/n for A
+        sample_A_count_div_n = prior_A_count[i]/prior_A_n[i]
+        sample_A_count_div_n_list.append(sample_A_count_div_n)
+        # calculate count/n for B
+        sample_B_count_div_n = prior_B_count[i]/prior_B_n[i]
+        sample_B_count_div_n_list.append(sample_B_count_div_n)
+    
+    # plot the distributions
+    sns.set(style='white', palette='muted', color_codes=True)
+    # Set up the matplotlib figure
+    dist_plot, axes = plt.subplots(figsize=(7, 7), sharex=True)
+    dist_plot.suptitle('Posterior distributions of $p_A$ (blue) and $p_B$ (red) after {} draws'.format(N_simulations))
+    sns.despine(left=True)
+    # posterior A
+    p1 = sns.distplot(sample_A_count_div_n_list, color='b', label='Posterior of $p_A$')
+    p1.vlines(sample_A_lpv, 0, lpv_height, colors='b', linestyle='--', label='Sample A LPV: {0:0.3f}'.format(sample_A_lpv))
+    p1.legend(loc='upper left')
+    # posterior B
+    p2 = sns.distplot(sample_B_count_div_n_list, color='r', label='Posterior of $p_B$')
+    p2.vlines(sample_B_lpv, 0, lpv_height, colors='r', linestyle='--', label='Sample B LPV: {0:0.3f}'.format(sample_B_lpv))
+    p2.legend(loc='upper left')
+    # display plot
+    plt.tight_layout() # fix any overlapping
+    
+    ###########################################################################
+    # get proportion of p_A_samples that are greater than p_B_samples
+    # iterate through p_A_samples and p_B_samples
+    sum_of_A_greater_than_B = 0
+    for i in range(len(sample_A_count_div_n_list)):
+        if sample_A_count_div_n_list[i] > sample_B_count_div_n_list[i]:
+            sum_of_A_greater_than_B += 1
+        
+    # calculate proportion A greater than B    
+    proportion_A_greater_than_B = sum_of_A_greater_than_B/len(sample_A_count_div_n_list)
+    # calculate proportion B greater than A
+    proportion_B_greater_than_A = 1-proportion_A_greater_than_B
+    
+    ###########################################################################
+    # put all of the objects we want inside of a class so they can be returned
+    class Attributes:
+        def __init__(self, df, lpv_plot, sample_A_count_div_n_list, sample_B_count_div_n_list, bfmi, max_gr, dist_plot, proportion_A_greater_than_B, proportion_B_greater_than_A):
+            self.df = df
+            self.lpv_plot = lpv_plot
+            self.sample_A_count_div_n_list = sample_A_count_div_n_list
+            self.sample_B_count_div_n_list = sample_B_count_div_n_list
+            self.bfmi = bfmi
+            self.max_gr = max_gr
+            self.dist_plot = dist_plot
+            self.proportion_A_greater_than_B = proportion_A_greater_than_B
+            self.proportion_B_greater_than_A = proportion_B_greater_than_A
+    
+    x = Attributes(df, lpv_plot, sample_A_count_div_n_list, sample_B_count_div_n_list, bfmi, max_gr, dist_plot, proportion_A_greater_than_B, proportion_B_greater_than_A)
+    return x
+
+###############################################################################
+###############################################################################
+
+# define function for testing probabilities
+def bayesian_ab_test_prob(sample_a_total, sample_a_responses, sample_b_total, sample_b_responses, N_simulations=1000, pct_tune=50, gr_threshold=1.001, N_additional_draws=1000, lpv_height=30):
     ###########################################################################
     # get parameters for model
     # make pct_tune into a proportion
@@ -170,11 +368,11 @@ def bayesian_ab_test_prob(sample_a_total, sample_a_responses, sample_b_total, sa
     sns.despine(left=True)
     # posterior A
     p1 = sns.distplot(p_A_samples, color='b', label='Posterior of $p_A$')
-    p1.vlines(sample_A_lpv, 0, 30, colors='b', linestyle='--', label='Sample A LPV: {0:0.3f}'.format(sample_A_lpv))
+    p1.vlines(sample_A_lpv, 0, lpv_height, colors='b', linestyle='--', label='Sample A LPV: {0:0.3f}'.format(sample_A_lpv))
     p1.legend(loc='upper left')
     # posterior B
     p2 = sns.distplot(p_B_samples, color='r', label='Posterior of $p_B$')
-    p2.vlines(sample_B_lpv, 0, 30, colors='r', linestyle='--', label='Sample B LPV: {0:0.3f}'.format(sample_B_lpv))
+    p2.vlines(sample_B_lpv, 0, lpv_height, colors='r', linestyle='--', label='Sample B LPV: {0:0.3f}'.format(sample_B_lpv))
     p2.legend(loc='upper left')
     # display plot
     plt.tight_layout() # fix any overlapping
